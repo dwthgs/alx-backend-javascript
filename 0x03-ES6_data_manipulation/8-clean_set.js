ae95f48dcfb9f6bab9353bf416ac41d7dff02c0a
dev <dev@devs.local>
2021-11-30 23:20:53 +0300

    cleanSet: returns a string of all the set values that start with a specific string (startString)

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') return '';
 
-  const strList = [];
+  let strList = '';
   for (const str of set) {
     if (typeof str === 'string' && str.startsWith(startString)) {
-      strList.push(str.slice(startString.length));
+      strList += `${str.slice(startString.length)}-`;
     }
   }
 
-  return strList.join('-');
+  return strList.slice(0, -1);
 }
