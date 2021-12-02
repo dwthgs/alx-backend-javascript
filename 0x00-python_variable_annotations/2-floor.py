3b69e4ddfcd34fc97810fc007623680a36d22ce9
dev <davicho2014@gmail.com>
2021-12-02 15:07:36 +0300

    Deleted PVA

--- 0x00-python_variable_annotations/2-floor.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""  """
-
-
-def floor(n: float) -> int:
-    """  """
-    return n.__floor__()
--- 0x00-python_variable_annotations/3-to_str.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""  """
-
-
-def to_str(n: float) -> str:
-    """  """
-    return n.__str__()
