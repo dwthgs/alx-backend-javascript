958c546646574bd0a07fb62368a007279c790dbf
dev <dev@devs.local>
2022-01-27 23:48:11 +0300

    -

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,13 +1,15 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const app = express();
 const port = 7865;
 
-app.get('/', (req, res) => res.end('Welcome to the payment system'))
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`)
-})
-app.get('/available_payments', (req, res) => {
+app.use(express.json());
+
+app.get("/", (req, res) => res.end("Welcome to the payment system"));
+app.get("/cart/:id([0-9]+)", (req, res) => {
+  res.end(`Payment methods for cart ${req.params.id}`);
+});
+app.get("/available_payments", (req, res) => {
   const obj = {
     payment_methods: {
       credit_cards: true,
@@ -16,7 +18,7 @@ app.get('/available_payments', (req, res) => {
   };
   res.json(obj);
 });
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   res.end(`Welcome ${req.body.userName}`);
 });
 app.listen(port, () => console.log(`API available on localhost port ${port}`));
